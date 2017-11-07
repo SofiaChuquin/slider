f762ddd6e9f9257be4eb4a4380b6c9ca209e4862
Sofia Chuquin <sofiachuquin@gmail.com>
2017-11-07 12:05:09 -0500

    Add slick

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/index.js'));
 app.get('/', function(req, res) {
 	res.render('index');
 });
